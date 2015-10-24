293fc39e6106fe27fb7e6131e93d15eb6e8eff50
Ike Mattice <gogreen37@me.com>
2015-10-24 11:43:10 -0700

    move all heroes to ng-repeat.

--- controllers/HeroesController.js
+++ controllers/HeroesController.js
@@ -2,6 +2,14 @@ dotaDraft.controller('HeroesCtrl', function HeroesCtrl($scope, HeroesFactory) {
     $scope.strHeroes = HeroesFactory.strHeroes;
     $scope.HeroesFactory = HeroesFactory;
 
-    $scope.rStrHeroes = HeroesFactory.rStrHeroes;
+    $scope.radientStrHeroes = HeroesFactory.radientStrHeroes;
+    $scope.direStrHeroes = HeroesFactory.direStrHeroes;
+
+    $scope.radientAgiHeroes = HeroesFactory.radientAgiHeroes;
+    $scope.direAgiHeroes = HeroesFactory.direAgiHeroes;
+
+    $scope.radientIntHeroes = HeroesFactory.radientIntHeroes;
+    $scope.direIntHeroes = HeroesFactory.direIntHeroes;
+
     // console.log('heroes' + $scope.heroes);
 })
--- services/HeroesFactory.js
+++ services/HeroesFactory.js
@@ -82,12 +82,372 @@ dotaDraft.factory('HeroesFactory', function HeroesFactory() {
             portriat: '../css/images/Str/Radient/Tusk.png',
         },
     ]
+    factory.direStrHeroes = [
+        {
+            name: 'Abaddon',
+            portriat: '../css/images/Str/Dire/Abaddon.png',
+        },
+        {
+            name: 'Axe',
+            portriat: '../css/images/Str/Dire/Axe.png',
+        },
+        {
+            name: 'Chaos Knight',
+            portriat: '../css/images/Str/Dire/Chaos_Knight.png',
+        },
+        {
+            name: 'Doom',
+            portriat: '../css/images/Str/Dire/Doom.png',
+        },
+        {
+            name: 'Lifestealer',
+            portriat: '../css/images/Str/Dire/Lifestealer.png',
+        },
+        {
+            name: 'Lycan',
+            portriat: '../css/images/Str/Dire/Lycan.png',
+        },
+        {
+            name: 'Magnus',
+            portriat: '../css/images/Str/Dire/Magnus.png',
+        },
+        {
+            name: 'Night Stalker',
+            portriat: '../css/images/Str/Dire/Night_Stalker.png',
+        },
+        {
+            name: 'Pudge',
+            portriat: '../css/images/Str/Dire/Pudge.png',
+        },
+        {
+            name: 'Sand King',
+            portriat: '../css/images/Str/Dire/Sand_King.png',
+        },
+        {
+            name: 'Slardar',
+            portriat: '../css/images/Str/Dire/Slardar.png',
+        },
+        {
+            name: 'Spirit Breaker',
+            portriat: '../css/images/Str/Dire/Spirit_Breaker.png',
+        },
+        {
+            name: 'Undying',
+            portriat: '../css/images/Str/Dire/Undying.png',
+        },
+        {
+            name: 'Wraith King',
+            portriat: '../css/images/Str/Dire/Wraith_King.png',
+        },
+    ]
 
-    factory.direStrHeroes = []
-    factory.radientAgiHeroes = []
-    factory.direAgiHeroes = []
-    factory.radientIntHeroes = []
-    factory.direIntHeroes = []
+    factory.radientAgiHeroes = [
+        {
+            name: 'Anti-Mage',
+            portriat: '../css/images/Agi/Radient/Anti-Mage.png',
+        },
+        {
+            name: 'Bounty Hunter',
+            portriat: '../css/images/Agi/Radient/Bounty_Hunter.png',
+        },
+        {
+            name: 'Drow Ranger',
+            portriat: '../css/images/Agi/Radient/Drow_Ranger.png',
+        },
+        {
+            name: 'Ember Spirit',
+            portriat: '../css/images/Agi/Radient/Ember_Spirit.png',
+        },
+        {
+            name: 'Gyrocopter',
+            portriat: '../css/images/Agi/Radient/Gyrocopter.png',
+        },
+        {
+            name: 'Juggernaut',
+            portriat: '../css/images/Agi/Radient/Juggernaut.png',
+        },
+        {
+            name: 'Lone Druid',
+            portriat: '../css/images/Agi/Radient/Lone_Druid.png',
+        },
+        {
+            name: 'Luna',
+            portriat: '../css/images/Agi/Radient/Luna.png',
+        },
+        {
+            name: 'Mirana',
+            portriat: '../css/images/Agi/Radient/Mirana.png',
+        },
+        {
+            name: 'Morphling',
+            portriat: '../css/images/Agi/Radient/Morphling.png',
+        },
+        {
+            name: 'Naga Siren',
+            portriat: '../css/images/Agi/Radient/Naga_Siren.png',
+        },
+        {
+            name: 'Phantom Lancer',
+            portriat: '../css/images/Agi/Radient/Phantom_Lancer.png',
+        },
+        {
+            name: 'Riki',
+            portriat: '../css/images/Agi/Radient/Riki.png',
+        },
+        {
+            name: 'Sniper',
+            portriat: '../css/images/Agi/Radient/Sniper.png',
+        },
+        {
+            name: 'Templar Assassin',
+            portriat: '../css/images/Agi/Radient/Templar_Assassin.png',
+        },
+        {
+            name: 'Troll Warlord',
+            portriat: '../css/images/Agi/Radient/Troll_Warlord.png',
+        },
+        {
+            name: 'Ursa',
+            portriat: '../css/images/Agi/Radient/Ursa.png',
+        },
+        {
+            name: 'Vengeful Spirit',
+            portriat: '../css/images/Agi/Radient/Vengeful_Spirit.png',
+        },
+    ]
+    factory.direAgiHeroes = [
+        {
+            name: 'Bloodseeker',
+            portriat: '../css/images/Agi/Dire/Bloodseeker.png',
+        },
+        {
+            name: 'Broodmother',
+            portriat: '../css/images/Agi/Dire/Broodmother.png',
+        },
+        {
+            name: 'Clinkz',
+            portriat: '../css/images/Agi/Dire/Clinkz.png',
+        },
+        {
+            name: 'Faceless Void',
+            portriat: '../css/images/Agi/Dire/Faceless_Void.png',
+        },
+        {
+            name: 'Medusa',
+            portriat: '../css/images/Agi/Dire/Medusa.png',
+        },
+        {
+            name: 'Meepo',
+            portriat: '../css/images/Agi/Dire/Meepo.png',
+        },
+        {
+            name: 'Nyx Assassin',
+            portriat: '../css/images/Agi/Dire/Nyx_Assassin.png',
+        },
+        {
+            name: 'Phantom Assassin',
+            portriat: '../css/images/Agi/Dire/Phantom_Assassin.png',
+        },
+        {
+            name: 'Razor',
+            portriat: '../css/images/Agi/Dire/Razor.png',
+        },
+        {
+            name: 'Shadow Fiend',
+            portriat: '../css/images/Agi/Dire/Shadow_Fiend.png',
+        },
+        {
+            name: 'Slark',
+            portriat: '../css/images/Agi/Dire/Slark.png',
+        },
+        {
+            name: 'Spectre',
+            portriat: '../css/images/Agi/Dire/Spectre.png',
+        },
+        {
+            name: 'Terrorblade',
+            portriat: '../css/images/Agi/Dire/Terrorblade.png',
+        },
+        {
+            name: 'Venomancer',
+            portriat: '../css/images/Agi/Dire/Venomancer.png',
+        },
+        {
+            name: 'Viper',
+            portriat: '../css/images/Agi/Dire/Viper.png',
+        },
+        {
+            name: 'Weaver',
+            portriat: '../css/images/Agi/Dire/Weaver.png',
+        },
+    ]
+
+    factory.radientIntHeroes = [
+        {
+            name: 'Chen',
+            portriat: '../css/images/Int/Radient/Chen.png',
+        },
+        {
+            name: 'Crystal Maiden',
+            portriat: '../css/images/Int/Radient/Crystal_Maiden.png',
+        },
+        {
+            name: 'Disruptor',
+            portriat: '../css/images/Int/Radient/Disruptor.png',
+        },
+        {
+            name: 'Enchantress',
+            portriat: '../css/images/Int/Radient/Enchantress.png',
+        },
+        {
+            name: 'Jakiro',
+            portriat: '../css/images/Int/Radient/Jakiro.png',
+        },
+        {
+            name: 'Keeper of the Light',
+            portriat: '../css/images/Int/Radient/Keeper_of_the_Light.png',
+        },
+        {
+            name: 'Lina',
+            portriat: '../css/images/Int/Radient/Lina.png',
+        },
+        {
+            name: 'Nature\'s Prophet',
+            portriat: '../css/images/Int/Radient/Natures_Prophet.png',
+        },
+        {
+            name: 'Ogre Magi',
+            portriat: '../css/images/Int/Radient/Ogre_Magi.png',
+        },
+        {
+            name: 'Oracle',
+            portriat: '../css/images/Int/Radient/Oracle.png',
+        },
+        {
+            name: 'Puck',
+            portriat: '../css/images/Int/Radient/Puck.png',
+        },
+        {
+            name: 'Rubick',
+            portriat: '../css/images/Int/Radient/Rubick.png',
+        },
+        {
+            name: 'Shadow Shaman',
+            portriat: '../css/images/Int/Radient/Shadow_Shaman.png',
+        },
+        {
+            name: 'Silencer',
+            portriat: '../css/images/Int/Radient/Silencer.png',
+        },
+        {
+            name: 'Skywrath Mage',
+            portriat: '../css/images/Int/Radient/Skywrath_Mage.png',
+        },
+        {
+            name: 'Storm Spirit',
+            portriat: '../css/images/Int/Radient/Storm_Spirit.png',
+        },
+        {
+            name: 'Techies',
+            portriat: '../css/images/Int/Radient/Techies.png',
+        },
+        {
+            name: 'Tinker',
+            portriat: '../css/images/Int/Radient/Tinker.png',
+        },
+        {
+            name: 'Windranger',
+            portriat: '../css/images/Int/Radient/Windranger.png',
+        },
+        {
+            name: 'Zeus',
+            portriat: '../css/images/Int/Radient/Zeus.png',
+        },
+
+    ]
+    factory.direIntHeroes = [
+        {
+            name: 'Ancient Apparition',
+            portriat: '../css/images/Int/Dire/Ancient_Apparition.png',
+        },
+        {
+            name: 'Bane',
+            portriat: '../css/images/Int/Dire/Bane.png',
+        },
+        {
+            name: 'Batrider',
+            portriat: '../css/images/Int/Dire/Batrider.png',
+        },
+        {
+            name: 'Dark Seer',
+            portriat: '../css/images/Int/Dire/Dark_Seer.png',
+        },
+        {
+            name: 'Dazzle',
+            portriat: '../css/images/Int/Dire/Dazzle.png',
+        },
+        {
+            name: 'Death Prophet',
+            portriat: '../css/images/Int/Dire/Death_Prophet.png',
+        },
+        {
+            name: 'Enigma',
+            portriat: '../css/images/Int/Dire/Enigma.png',
+        },
+        {
+            name: 'Invoker',
+            portriat: '../css/images/Int/Dire/Invoker.png',
+        },
+        {
+            name: 'Leshrac',
+            portriat: '../css/images/Int/Dire/Leshrac.png',
+        },
+        {
+            name: 'Lich',
+            portriat: '../css/images/Int/Dire/Lich.png',
+        },
+        {
+            name: 'Lion',
+            portriat: '../css/images/Int/Dire/Lion.png',
+        },
+        {
+            name: 'Necrophos',
+            portriat: '../css/images/Int/Dire/Necrophos.png',
+        },
+        {
+            name: 'Outworld Devourer',
+            portriat: '../css/images/Int/Dire/Outworld_Devourer.png',
+        },
+        {
+            name: 'Pugna',
+            portriat: '../css/images/Int/Dire/Pugna.png',
+        },
+        {
+            name: 'Queen of Pain',
+            portriat: '../css/images/Int/Dire/Queen_of_Pain.png',
+        },
+        {
+            name: 'Shadow Demon',
+            portriat: '../css/images/Int/Dire/Shadow_Demon.png',
+        },
+        {
+            name: 'Visage',
+            portriat: '../css/images/Int/Dire/Visage.png',
+        },
+        {
+            name: 'Warlock',
+            portriat: '../css/images/Int/Dire/Warlock.png',
+        },
+        {
+            name: 'Winter Wyvern',
+            portriat: '../css/images/Int/Dire/Winter_Wyvern.png',
+        },
+        {
+            name: 'Witch Doctor',
+            portriat: '../css/images/Int/Dire/Witch_Doctor.png',
+        },
+
+    ]
 
     return factory;
 })
